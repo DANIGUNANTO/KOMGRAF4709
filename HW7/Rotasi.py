e3dc96d80abe526d6f7dbc9c0c2cfeaac7525c37
DANIGUNANTO <115636511+DANIGUNANTO@users.noreply.github.com>
2023-11-29 22:12:57 +0700

    Update Rotasi.cpp

--- HW7/Rotasi.py
+++ HW7/Rotasi.py
@@ -1,67 +1,70 @@
-import matplotlib.pyplot as plt
-import numpy as np
+#include <GL/glut.h>
+#include <cmath>
 
-def rotate(matrix, angle):
-  
+float angle = 30.0f; // Rotation angle in degrees
 
-  rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
-                                [np.sin(angle), np.cos(angle)]])
-  return np.matmul(matrix, rotation_matrix)
+void drawLine(float x1, float y1, float x2, float y2) {
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
+}
 
-def translate(matrix, translation):
- 
+void rotatePoint(float& x, float& y, float angle, float pivotX, float pivotY) {
+    // Convert angle from degrees to radians
+    float radian = angle * (M_PI / 180.0);
 
-  translated_matrix = np.empty_like(matrix)
-  for i in range(matrix.shape[0]):
-    translated_matrix[i] = matrix[i] + translation
-  return translated_matrix
+    // Translate rotation center to the initial point
+    x -= pivotX;
+    y -= pivotY;
 
-def connect_points(points, color):
-  
+    // Rotate around the rotation center
+    float rotatedX = x * cos(radian) - y * sin(radian);
+    float rotatedY = x * sin(radian) + y * cos(radian);
 
-  plt.plot([p[0] for p in points], [p[1] for p in points] , '-', color=color)
+    // Translate back to the initial position
+    x = rotatedX + pivotX;
+    y = rotatedY + pivotY;
+}
 
-def plot_points(points, color='black'):
- 
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  plt.plot([p[0] for p in points], [p[1] for p in points], 'o', color=color)
+    glColor3f(1.0f, 0.0f, 0.0f); // Red color
 
-def plot_grid(xmin, xmax, ymin, ymax):
-  
+    float x1 = 1.0f, y1 = 3.0f, x2 = 6.0f, y2 = 8.0f;
 
-  plt.xticks(np.arange(xmin, xmax + 1))
-  plt.yticks(np.arange(ymin, ymax + 1))
-  plt.grid(True)
-  plt.scatter([p[0] for p in points], [p[1] for p in points])
-  for i in range(xmin, xmax + 1):
-    plt.annotate(str(i), (i, 0.5), ha='center', va='bottom')
-  for i in range(ymin, ymax + 1):
-    plt.annotate(str(i), (0.5, i), ha='center', va='center')
+    // Original line before rotation
+    drawLine(x1, y1, x2, y2);
 
-# Matriks titik-titik
-points = np.array([[1, 3], [3, 7]])
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue color
 
-# Sudut rotasi
-angle = np.pi / 4
+    // Rotate the line endpoints before drawing the rotated line
+    rotatePoint(x1, y1, angle, 3.0f, 3.0f);
+    rotatePoint(x2, y2, angle, 3.0f, 3.0f);
 
-# Matriks rotasi
-rotated_points = rotate(points, angle)
+    // Rotated line
+    drawLine(x1, y1, x2, y2);
 
-# Hubungkan titik-titik sebelum rotasi dengan garis berwarna hitam
-connect_points(points, color='black')
+    glutSwapBuffers();
+}
 
-# Hubungkan titik-titik sesudah rotasi dengan garis berwarna merah
-connect_points(rotated_points, color='red')
+void init() {
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 10.0, 0.0, 10.0); // Window coordinates
+}
 
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("OpenGL Rotasi Garis");
 
-# Tampilkan grafik titik-titik sebelum dan sesudah rotasi
-plot_points(points, color='black')
-plot_points(rotated_points, color='red')
+    init();
 
-# Tampilkan grid pada grafik
-plot_grid(0, 10, 0, 10)
+    glutDisplayFunc(display);
+    glutMainLoop();
 
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Grafik Rotasi')
-plt.show()
+    return 0;
+}
+
